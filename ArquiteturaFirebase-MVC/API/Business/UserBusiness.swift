7345ae794580d0057afe46070ada364e63c21858
Oren Pelosof <orenpelosof@gmail.com>
2023-09-07 19:54:21 -0300

    Classe Business implementada

--- ArquiteturaFirebase-MVC/API/Business/UserBusiness.swift
+++ ArquiteturaFirebase-MVC/API/Business/UserBusiness.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+protocol UserBusinessProtocol {
+    func login(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void)
+    func register(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void)
+    
+}
+class UserBusiness: UserBusinessProtocol {
+    let provider = UserProvider()
+    
+    func login(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        
+        self.provider.login(parameters: getParams(email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHandler(.success(userModel))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+        
+    }
+    
+    func register(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        
+        self.provider.register(parameters: getParams(email, password)) {result in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let userModel):
+                completionHandler(.success(userModel))
+            }
+        }
+    }
+    
+    // MARK: - Private methods
+    
+    private func getParams(_ email: String,_ password: String) -> [AnyHashable: Any] {
+        let userModel = UserModel(email: email, password: password)
+        let params: [AnyHashable: Any] = [Constants.ParametersKeys.body: [Constants.ParametersKeys.userModel: userModel]]
+        
+        return params
+        
+        
+    }
+}
